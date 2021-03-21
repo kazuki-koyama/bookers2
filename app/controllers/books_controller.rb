cf45f5c45afd1afc796caeac16f031ecab7873f7
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-21 00:18:55 +0000

    [Update] createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
@@ -10,6 +12,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
