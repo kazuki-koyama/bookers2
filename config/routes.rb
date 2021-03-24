e8f3a9de8792d9fd6fd10b80ab74787d15c8e65d
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-24 22:21:56 +0000

    [Update] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users, only:[:index, :show, :edit, :update]
-  resources :books
+  resources :users, only:[:index, :show, :edit, :update] do
+    resources :books
+  end
 end
