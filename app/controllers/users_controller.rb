6f18f1212e5a7e45011bbcc29ee76b7343e90d82
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-24 22:01:27 +0000

    [Add] branch分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = Book.new
+    @book = Book.find(params[:id])
+    @book.user_id = @user.id
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render :edit
+    else
+      redirect_to books_path(current_user.id)
+    end
   end
   def update
     user = User.find(params[:id])
