5d067a3ca3016d753590f7b708175619ceac9dcc
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-03-25 04:13:14 +0000

    [Fix] books#updateのインスタンス変数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,9 +30,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book), notice: "You have updated book successfully"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully"
     else
       render :edit
     end
